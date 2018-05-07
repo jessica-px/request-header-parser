48151a690078908bef959461b9e2d664c6b1fbb9
Jessica <jessypeck@gmail.com>
2018-05-07 12:04:51 -0700

    Created header parser

--- server.js
+++ server.js
@@ -5,7 +5,36 @@ const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify(req.headers));
+  const headerInfo = parseHeader(req);
+  res.send(JSON.stringify(headerInfo));
 })
 
+const parseHeader = (req) => {
+  const headerInfo = {
+    'ip-address': parseIP(req),
+    'language': parseLangage(req),
+    'OS': parseOS(req)
+  }
+  return headerInfo;
+}
+
+const parseIP = (req) => {
+  let ip = req.header("x-forwarded-for");
+  if (ip == null){
+    ip = 'local'
+  }
+  return ip;
+}
+
+const parseLangage = (req) => {
+  const input = req.header("accept-language");
+  return input.slice(0, 2);
+}
+
+const parseOS = (req) => {
+  const input = req.header("user-agent");
+  return input.match(/(?<=\().*?(?=\))/)[0]; // first set of characters enclosed in parentheses
+}
+
 app.listen(port);
+
